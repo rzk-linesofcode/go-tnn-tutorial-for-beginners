d8368ad364d212b89b47f31e96b4d4c3140a29eb
rezky <rzk84.rezky@gmail.com>
2021-07-18 17:42:47 +0700

    v1.0

--- test.go
+++ test.go
@@ -4,6 +4,42 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, Ninjas!")
+	// strings
+	// var nameOne string = "mario"
+	// var nameTwo = "luigi"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameOne = "peach"
+	// nameThree = "bowser"
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameFour := "yoshi"
+
+	// fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+
+	// ints
+
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits and memory
+
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+	// var numThree uint8 = 25
+
+	// float
+
+	var scoreOne float32 = -1.5
+	var scoreTwo float64 = 8465826345620645072.68
+	scoreThree := 50.5
+
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
 
 }
