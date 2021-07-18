7c6a6b3ab33eeb491bb3ef9fbd949fb2dc545020
rezky <rzk84.rezky@gmail.com>
2021-07-18 20:56:17 +0700

    2021-07-18

--- test.go
+++ test.go
@@ -1,9 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 
-	fmt.Println("Hello, Ninjas!")
+	// greeting := "hello there friend!"
+	// fmt.Println(strings.Contains(greeting, "hello"))
+	// fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+	// fmt.Println(strings.ToUpper(greeting))
+	// fmt.Println(strings.Index(greeting, "th"))
+	// fmt.Println(strings.Split(greeting, " "))
+
+	// the original value is unchange
+	// fmt.Println("original string value =", greeting)
+
+	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
+
+	sort.Ints(ages)
+	fmt.Println(ages)
+
+	index := sort.SearchInts(ages, 30)
+	fmt.Println(index)
+
+	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
+	sort.Strings(names)
+
+	fmt.Println(names)
+
+	fmt.Println(sort.SearchStrings(names, "bowser"))
 
 }
