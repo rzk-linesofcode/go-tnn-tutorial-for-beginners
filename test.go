f210759e9ac77984090c523c9a98460161491399
rezky <rzk84.rezky@gmail.com>
2021-07-20 21:15:32 +0700

    2021-07-20

--- test.go
+++ test.go
@@ -4,6 +4,34 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, Ninjas!")
+	age := 45
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("age is less than 40")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 45")
+	}
+
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
+
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("breaking at index", index)
+			break
+		}
+
+		fmt.Printf("the value at pos %v is %v \n", index, value)
+	}
 
 }
