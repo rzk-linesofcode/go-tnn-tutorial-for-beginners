8a0fd3f24098b696882c196dd5578c6ffcff92f4
rezky <rzk84.rezky@gmail.com>
2021-07-20 21:52:42 +0700

    2021-07-20

--- test.go
+++ test.go
@@ -1,9 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func getInitials(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
+
+	fmt.Println(names)
+
+	var initials []string
+
+	for _, v := range names {
+		fmt.Println(v[:1])
+		initials = append(initials, v[:1])
+
+	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], "_"
+
+}
 
 func main() {
 
-	fmt.Println("Hello, Ninjas!")
+	fn1, sn1 := getInitials("tifa lockhart")
+	fmt.Println(fn1, sn1)
+
+	fn2, sn2 := getInitials("cloud strife")
+	fmt.Println(fn2, sn2)
+
+	fn3, sn3 := getInitials("barret")
+	fmt.Println(fn3, sn3)
 
 }
