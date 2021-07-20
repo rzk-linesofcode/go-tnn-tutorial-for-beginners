2ec595a1c478f56dc42b93b7be0ca960278e6c76
rezky <rzk84.rezky@gmail.com>
2021-07-20 21:03:00 +0700

    2021-07-20

--- test.go
+++ test.go
@@ -4,6 +4,32 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, Ninjas!")
+	//x := 0
+
+	// for x < 5 {
+	// 	fmt.Println("value x is", x)
+	// 	x++
+	// }
+
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("value i is", i)
+	// }
+
+	names := []string{"mario", "luigi", "yoshi", "peach"}
+
+	// for i := 0; i < len(names); i++ {
+	// 	fmt.Println(names[i])
+	// }
+
+	// for index, value := range names {
+	// 	fmt.Printf("the value at index %v is %v \n", index, value)
+	// }
+
+	for _, value := range names {
+		fmt.Printf("the value is %v \n", value)
+		value = "new string"
+	}
+
+	fmt.Println(names)
 
 }
