d76583b278f924bf7b7e0672a659dd7e51385880
rezky <rzk84.rezky@gmail.com>
2021-07-22 22:40:01 +0700

    2021-07-22

--- test.go
+++ test.go
@@ -2,8 +2,24 @@ package main
 
 import "fmt"
 
+func updateName(x *string) {
+	*x = "wedge"
+}
+
 func main() {
 
-	fmt.Println("Hello, Ninjas!")
+	name := "tifa"
+
+	//updateName(name)
+
+	//fmt.Println("memory address of name is:", &name)
+
+	m := &name
+	// fmt.Println("memory address (value of m):", m)
+	// fmt.Println("value of memory address:", *m)
+
+	fmt.Println(name)
+	updateName(m)
+	fmt.Println(name)
 
 }
