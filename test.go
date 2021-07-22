b0c5331e25962423084fd963cc39588eeee33f56
rezky <rzk84.rezky@gmail.com>
2021-07-22 22:24:50 +0700

    2021-07-22

--- test.go
+++ test.go
@@ -2,8 +2,30 @@ package main
 
 import "fmt"
 
+func updateName(x string) string {
+	x = "wedge"
+	return x
+}
+
+func updateMenu(y map[string]float64) {
+	y["coffee"] = 2.99
+}
+
 func main() {
 
-	fmt.Println("Hello, Ninjas!")
+	// group A types (non-pointer value) -> strings, ints, books, floats, arrays, structs
+	name := "tifa"
+
+	name = updateName(name)
+	fmt.Println(name)
+
+	// group b types (pointer value) -> slices, maps, functions
+	menu := map[string]float64{
+		"pie":       5.95,
+		"ice cream": 3.99,
+	}
+
+	updateMenu(menu)
+	fmt.Println(menu)
 
 }
